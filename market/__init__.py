9aa55eb34b587170bca7139dd6ca0baed58e299e
Ida Delphine <idadelm@gmail.com>
2022-04-03 18:35:12 +0100

    Enable user login

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'fd6a4cb71746e00b0b67b287'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
