e7b3cebeaa32a478da2be60be00baa04949634cd
Ida Delphine <idadelm@gmail.com>
2022-04-03 14:40:42 +0100

    Created register form

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'fd6a4cb71746e00b0b67b287'
 db = SQLAlchemy(app)
 
 from market import routes
