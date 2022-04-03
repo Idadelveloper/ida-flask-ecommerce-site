be6e3947f4f086b41b17512c85ecb689c1ca0962
Ida Delphine <idadelm@gmail.com>
2022-04-03 20:32:31 +0100

    Implemented logout and modified homepage

--- market/__init__.py
+++ market/__init__.py
@@ -10,5 +10,7 @@ app.config['SECRET_KEY'] = 'fd6a4cb71746e00b0b67b287'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -3,7 +3,7 @@ from flask import flash, render_template, redirect, url_for
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route("/")
@@ -13,6 +13,7 @@ def home_page():
 
 
 @app.route("/market")
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -27,6 +28,10 @@ def register_page():
         password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+
+        login_user(user_to_create)
+        flash(f'Account created successfully! You are now logged in as {user_to_create.username}', category="success")
+
         return redirect(url_for('market_page'))
 
     # If there are no validation errors
@@ -50,3 +55,11 @@ def login_page():
         flash('Username and password are not matched! Please try again', category='danger')
 
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for("home_page"))
+
