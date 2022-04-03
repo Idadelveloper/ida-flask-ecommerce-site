222977d8d7f18cce54f528adb727304f76f2ac4d
Ida Delphine <idadelm@gmail.com>
2022-04-03 16:50:49 +0100

    hash passwords

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'fd6a4cb71746e00b0b67b287'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
